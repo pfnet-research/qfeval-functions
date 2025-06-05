4d1721b6a697bcea815171bed242cded3dc2706e
Masanori Hirano <masa.hirano.1996@gmail.com>
2025-06-05 15:06:59 +0900

    fix

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,7 +4,7 @@ import os.path
 import qfeval_functions
 
 
-def test_version():
+def test_version() -> None:
     parser = configparser.ConfigParser()
     with open(
         os.path.join(os.path.dirname(__file__), "..", "pyproject.toml"), "r"
