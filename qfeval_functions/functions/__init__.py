452ee34d41c5d91a372c0a674879158447e6a805
imos <173752+imos@users.noreply.github.com>
2025-07-03 02:19:03 +0900

    .

--- qfeval_functions/functions/__init__.py
+++ qfeval_functions/functions/__init__.py
@@ -50,6 +50,7 @@ from .randint import randint
 from .randn import randn
 from .randn_like import randn_like
 from .randperm import randperm
+from .rci import rci
 from .rcummax import rcummax
 from .rcumsum import rcumsum
 from .rms import rms
@@ -114,6 +115,7 @@ __all__ = [
     "randn",
     "randn_like",
     "randperm",
+    "rci",
     "rcummax",
     "rcumsum",
     "reduce_nan_patterns",
